537001471a4eb69f820bf75a7484a86dd457fe2e
antonnifo <antonnifo@live.com>
2020-02-17 08:24:51 +0300

    register admin model

--- src/front_end/admin.py
+++ src/front_end/admin.py
@@ -2,11 +2,15 @@ from django.contrib import admin
 from .models import Contacts
 
 class ContactAdmin(admin.ModelAdmin):
-    list_display       = ('id', 'first_name', 'last_name', 'email','contact_date')
-    list_display_links = ('id','first_name')
-    # search_fields      = ('name','email','listing')
+    list_display       = ('id', 'name', 'email','contact_date')
+    list_display_links = ('id','name')
+    list_editable      = ('email', )
+    list_filter        = ('contact_date', )
+    search_fields      = ('last_name','email')
     list_per_page      = 25
 
+    def name (self,obj):
+        return f"{obj.first_name} {obj.last_name}"
 
-
-admin.site.register(Contacts,ContactAdmin)   
+admin.site.register(Contacts,ContactAdmin)
+# admin.site.site_header = "Wakali  Kwanza"   
